9845e2dd09db22d194c7934ee510186ecd0632de
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2016-10-12 11:17:19 +0530

    Load plugin css

--- includes/scripts.php
+++ includes/scripts.php
@@ -18,11 +18,20 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 function give_email_reports_admin_scripts( $hook ) {
 
-	// Use minified libraries if SCRIPT_DEBUG is turned off
+	// Use minified libraries if SCRIPT_DEBUG is turned off.
 	if ( $hook == 'give_forms_page_give-settings' ) {
+
+		/**
+		 * Scripts.
+		 */
 		wp_register_script( 'give_email_reports_admin_js', GIVE_EMAIL_REPORTS_URL . '/assets/js/admin.js', array( 'jquery' ) );
 		wp_enqueue_script( 'give_email_reports_admin_js' );
 
+		/**
+		 * Styles.
+		 */
+		wp_register_style( 'give_email_reports_admin_css', GIVE_EMAIL_REPORTS_URL . '/assets/css/admin.css' );
+		wp_enqueue_style( 'give_email_reports_admin_css' );
 	}
 }
 
