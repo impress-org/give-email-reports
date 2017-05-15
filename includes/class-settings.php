c2d959a11d8c2e3ae13dd9d972dde5de9f0c7fdb
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2017-05-15 16:11:24 +0530

    Add: register weekly email

--- includes/class-settings.php
+++ includes/class-settings.php
@@ -16,11 +16,7 @@ class Give_Email_Reports_Settings extends Give_Email_Reports {
 		add_action( 'cmb2_render_email_report_preview', array( $this, 'add_email_report_preview' ), 10, 5 );
 
 		add_action( 'give_admin_field_email_report_daily_schedule', array( $this, 'add_email_report_daily_schedule' ), 10, 5 );
-
-		add_action( 'cmb2_render_email_report_weekly_schedule', array(
-			$this,
-			'add_email_report_weekly_schedule',
-		), 10, 5 );
+		add_action( 'give_admin_field_email_report_weekly_schedule', array( $this, 'add_email_report_weekly_schedule' ), 10, 5 );
 
 		add_action( 'cmb2_render_email_report_monthly_schedule', array(
 			$this,
@@ -40,6 +36,7 @@ class Give_Email_Reports_Settings extends Give_Email_Reports {
 	 */
 	public function register_emails( $emails ) {
 		$emails[] = include GIVE_EMAIL_REPORTS_DIR . 'includes/emails/class-daily-report-email.php';
+		$emails[] = include GIVE_EMAIL_REPORTS_DIR . 'includes/emails/class-weekly-report-email.php';
 
 		return $emails;
 	}
@@ -191,13 +188,10 @@ class Give_Email_Reports_Settings extends Give_Email_Reports {
 	/**
 	 * Give add Weekly email reports preview.
 	 *
-	 * @param $field
-	 * @param $value
-	 * @param $object_id
-	 * @param $object_type
-	 * @param $field_type CMB2_Types
+	 * @param array $field
+	 * @param string $value
 	 */
-	public function add_email_report_weekly_schedule( $field, $value, $object_id, $object_type, $field_type ) {
+	public function add_email_report_weekly_schedule( $field, $value ) {
 		// Setting attribute.
 		$disabled_field = $this->is_cron_enabled( 'give_email_reports_weekly_email' ) ? ' disabled="disabled"' : '';
 
@@ -216,42 +210,52 @@ class Give_Email_Reports_Settings extends Give_Email_Reports {
 			'7' => 'Sunday',
 		);
 
-		ob_start(); ?>
-		<div class="give-email-reports-weekly">
-			<label class="hidden"
-			       for="<?php echo "{$field->args['id']}_day"; ?>"><?php _e( 'Day of Week', 'give-email-reports' ); ?></label>
+		ob_start();
+		?>
+		<tr valign="top">
+			<?php if ( ! empty( $field['name'] ) && ! in_array( $field['name'], array( '&nbsp;' ) ) ) : ?>
+				<th scope="row" class="titledesc">
+					<label for="<?php echo esc_attr( $field['name'] ); ?>"><?php echo $field['title']; ?></label>
+				</th>
+			<?php endif; ?>
+			<td class="give-forminp">
+				<div class="give-email-reports-weekly">
+					<label class="hidden"
+						   for="<?php echo "{$field['id']}_day"; ?>"><?php _e( 'Day of Week', 'give-email-reports' ); ?></label>
 
-			<select class="cmb2_select"
-			        name="<?php echo "{$field->args['id']}[day]"; ?> id="<?php echo "{$field->args['id']}_day"; ?>
-			"<?php echo $disabled_field; ?>>
-			<?php
-			// Day select dropdown.
-			foreach ( $days as $day_code => $day ) {
-				$value['day'] = isset( $value['day'] ) ? $value['day'] : 'sunday';
-				echo '<option value="' . $day_code . '" ' . selected( $value['day'], $day_code, true ) . '>' . $day . '</option>';
-			} ?>
-			</select>
+					<select class="cmb2_select"
+							name="<?php echo "{$field['id']}[day]"; ?> id="<?php echo "{$field['id']}_day"; ?>
+					"<?php echo $disabled_field; ?>>
+					<?php
+					// Day select dropdown.
+					foreach ( $days as $day_code => $day ) {
+						$value['day'] = isset( $value['day'] ) ? $value['day'] : 'sunday';
+						echo '<option value="' . $day_code . '" ' . selected( $value['day'], $day_code, true ) . '>' . $day . '</option>';
+					} ?>
+					</select>
 
-			<label class="hidden"
-			       for="<?php echo "{$field->args['id']}_time"; ?>'"><?php _e( 'Time of Day', 'give-email-reports' ); ?></label>
+					<label class="hidden"
+						   for="<?php echo "{$field['id']}_time"; ?>'"><?php _e( 'Time of Day', 'give-email-reports' ); ?></label>
 
-			<select class="cmb2_select" name="<?php echo "{$field->args['id']}[time]"; ?>"
-			        id="<?php echo "{$field->args['id']}_time"; ?>"<?php echo $disabled_field; ?>>
-				<?php
-				// Time select options.
-				foreach ( $times as $military => $time ) {
-					$value['time'] = isset( $value['time'] ) ? $value['time'] : '1900';
-					echo '<option value="' . $military . '" ' . selected( $value['time'], $military, false ) . '>' . $time . '</option>';
-				} ?>
-			</select>
+					<select class="cmb2_select" name="<?php echo "{$field['id']}[time]"; ?>"
+							id="<?php echo "{$field['id']}_time"; ?>"<?php echo $disabled_field; ?>>
+						<?php
+						// Time select options.
+						foreach ( $times as $military => $time ) {
+							$value['time'] = isset( $value['time'] ) ? $value['time'] : '1900';
+							echo '<option value="' . $military . '" ' . selected( $value['time'], $military, false ) . '>' . $time . '</option>';
+						} ?>
+					</select>
 
-			<?php $this->print_reset_button( 'give_email_reports_weekly_email' ); ?>
+					<?php $this->print_reset_button( 'give_email_reports_weekly_email' ); ?>
 
-			<p class="cmb2-metabox-description"><?php _e( 'Select the day of the week and time that you would like to receive the weekly report.', 'give-email-reports' ); ?></p>
+					<p class="give-field-description"><?php _e( 'Select the day of the week and time that you would like to receive the weekly report.', 'give-email-reports' ); ?></p>
 
-		</div>
-
-		<?php echo ob_get_clean();
+				</div>
+			</td>
+		</tr>
+		<?php
+		echo ob_get_clean();
 	}
 
 
---
+++ includes/emails/class-weekly-report-email.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * This file has code to handle weekly email reports notification.
+ */
+class Give_Weekly_Email_Notification extends Give_Email_Notification {
+	function init() {
+		$this->load( array(
+			'id'                    => 'weekly-report',
+			'label'                 => __( 'Weekly Email Report', 'give' ),
+			'description'           => __( '', 'give' ),
+			'notification_status'   => 'disabled',
+			'content_type_editable' => false,
+			'content_type'          => 'text/html',
+			// 'form_metabox_setting' => true,
+			'has_recipient_field'   => true,
+			'default_email_subject' => sprintf( __( 'Weekly Donation Report for %1$s', 'give-email-reports' ), get_bloginfo( 'name' ) ),
+			'default_email_message' => $this->get_default_email_message(),
+		) );
+	}
+
+
+	/**
+	 * Get extra setting field.
+	 *
+	 * @access public
+	 *
+	 * @param null $form_id
+	 *
+	 * @return array
+	 */
+	public function get_extra_setting_fields( $form_id = null ) {
+		return array(
+			array(
+				'id'   => 'give_email_reports_weekly_email_delivery_time',
+				'name' => __( 'Weekly Email Delivery Time', 'give-email-reports' ),
+				'desc' => __( 'Select when you would like to receive your weekly email report.', 'give-email-reports' ),
+				'type' => 'email_report_weekly_schedule',
+			)
+		);
+	}
+
+
+	/**
+	 * Get default email message
+	 *
+	 * @access public
+	 * @return string
+	 */
+	public function get_default_email_message() {
+		// $message will be rendered during give_email_message filter.
+		ob_start();
+		give_get_template_part( 'emails/body-report-weekly', Give()->emails->get_template(), true );
+
+		return ob_get_clean();
+	}
+}
+
+return Give_Weekly_Email_Notification::get_instance();
