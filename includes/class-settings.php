008b0b82e44c36597eb1614e81700b7c580e2e98
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2017-05-15 16:22:07 +0530

    Add: monthly email report

--- includes/class-settings.php
+++ includes/class-settings.php
@@ -12,17 +12,9 @@ class Give_Email_Reports_Settings extends Give_Email_Reports {
 		add_filter( 'give_email_notifications', array( $this, 'register_emails' ) );
 		add_action( 'init', array( $this, 'register_email_tags' ) );
 
-		// Register settings.
-		add_filter( 'give_settings_emails', array( $this, 'settings' ), 1 );
-		add_action( 'cmb2_render_email_report_preview', array( $this, 'add_email_report_preview' ), 10, 5 );
-
 		add_action( 'give_admin_field_email_report_daily_schedule', array( $this, 'add_email_report_daily_schedule' ), 10, 5 );
 		add_action( 'give_admin_field_email_report_weekly_schedule', array( $this, 'add_email_report_weekly_schedule' ), 10, 5 );
-
-		add_action( 'cmb2_render_email_report_monthly_schedule', array(
-			$this,
-			'add_email_report_monthly_schedule',
-		), 10, 5 );
+		add_action( 'give_admin_field_email_report_monthly_schedule', array( $this, 'add_email_report_monthly_schedule' ), 10, 5 );
 
 	}
 
@@ -38,6 +30,7 @@ class Give_Email_Reports_Settings extends Give_Email_Reports {
 	public function register_emails( $emails ) {
 		$emails[] = include GIVE_EMAIL_REPORTS_DIR . 'includes/emails/class-daily-report-email.php';
 		$emails[] = include GIVE_EMAIL_REPORTS_DIR . 'includes/emails/class-weekly-report-email.php';
+		$emails[] = include GIVE_EMAIL_REPORTS_DIR . 'includes/emails/class-monthly-report-email.php';
 
 		return $emails;
 	}
@@ -101,97 +94,6 @@ class Give_Email_Reports_Settings extends Give_Email_Reports {
 		foreach ( $email_tags as $email_tag ) {
 			give_add_email_tag( $email_tag['tag'], $email_tag['description'], $email_tag['func'], $email_tag['context'] );
 		}
-	}
-
-	/**
-	 * Add settings.
-	 *
-	 * @access      public
-	 * @since       1.0
-	 *
-	 * @param       array $settings The existing Give settings array.
-	 *
-	 * @return      array The modified Give settings array.
-	 */
-	public function settings( $settings ) {
-
-		$email_reports_settings = array(
-			array(
-				'name' => __( 'Email Reports Settings', 'give-email-reports' ),
-				'desc' => '<hr>',
-				'id'   => 'give_title',
-				'type' => 'give_title',
-			),
-			array(
-				'id'   => 'email_reports_settings',
-				'name' => __( 'Preview Reports', 'give-email-reports' ),
-				'desc' => '',
-				'type' => 'email_report_preview',
-			),
-			array(
-				'name'              => __( 'Report Frequency', 'give-email-reports' ),
-				'desc'              => __( 'Select the time frames that you would like to receive email reports.', 'give-email-reports' ),
-				'id'                => 'email_report_emails',
-				'type'              => 'multicheck',
-				'select_all_button' => false,
-				'options'           => array(
-					'daily'   => 'Daily',
-					'weekly'  => 'Weekly',
-					'monthly' => 'Monthly',
-				),
-			),
-			array(
-				'id'          => 'give_email_reports_daily_email_delivery_time',
-				'name'        => __( 'Daily Email Delivery Time', 'give-email-reports' ),
-				'desc'        => __( 'Select when you would like to receive your daily email report.', 'give-email-reports' ),
-				'type'        => 'email_report_daily_schedule',
-				'row_classes' => 'cmb-type-email-report-daily-schedule',
-				'default'     => '1900',
-			),
-			array(
-				'id'   => 'give_email_reports_weekly_email_delivery_time',
-				'name' => __( 'Weekly Email Delivery Time', 'give-email-reports' ),
-				'desc' => __( 'Select when you would like to receive your weekly email report.', 'give-email-reports' ),
-				'type' => 'email_report_weekly_schedule',
-			),
-			array(
-				'id'   => 'give_email_reports_monthly_email_delivery_time',
-				'name' => __( 'Monthly Email Delivery Time', 'give-email-reports' ),
-				'desc' => __( 'Select when you would like to receive your monthly email report.', 'give-email-reports' ),
-				'type' => 'email_report_monthly_schedule',
-			),
-		);
-
-		return array_merge( $settings, $email_reports_settings );
-	}
-
-	/**
-	 * Give add email reports preview.
-	 *
-	 * @since 1.0
-	 */
-	public function add_email_report_preview() {
-		ob_start();
-		?>
-		<a href="<?php echo esc_url( add_query_arg( array(
-			'give_action' => 'preview_email_report',
-			'report'      => 'daily',
-		), home_url() ) ); ?>"
-		   class="button-secondary" target="_blank"
-		   title="<?php _e( 'Preview Daily Report', 'give-email-reports' ); ?> "><?php _e( 'Preview Daily Report', 'give-email-reports' ); ?></a>
-		<a href="<?php echo esc_url( add_query_arg( array(
-			'give_action' => 'preview_email_report',
-			'report'      => 'weekly',
-		), home_url() ) ); ?>"
-		   class="button-secondary" target="_blank"
-		   title="<?php _e( 'Preview Weekly Report', 'give-email-reports' ); ?> "><?php _e( 'Preview Weekly Report', 'give-email-reports' ); ?></a>
-		<a href="<?php echo esc_url( add_query_arg( array(
-			'give_action' => 'preview_email_report',
-			'report'      => 'monthly',
-		), home_url() ) ); ?>"
-		   class="button-secondary" target="_blank"
-		   title="<?php _e( 'Preview Monthly Report', 'give-email-reports' ); ?> "><?php _e( 'Preview Monthly Report', 'give-email-reports' ); ?></a>
-		<?php echo ob_get_clean();
 	}
 
 	/**
@@ -324,13 +226,10 @@ class Give_Email_Reports_Settings extends Give_Email_Reports {
 	/**
 	 * Give add Monthly email reports preview.
 	 *
-	 * @param $field
-	 * @param $value
-	 * @param $object_id
-	 * @param $object_type
-	 * @param $field_type CMB2_Types
+	 * @param array $field
+	 * @param array $value
 	 */
-	public function add_email_report_monthly_schedule( $field, $value, $object_id, $object_type, $field_type ) {
+	public function add_email_report_monthly_schedule( $field, $value ) {
 		// Setting attribute.
 		$disabled_field = $this->is_cron_enabled( 'give_email_reports_monthly_email' ) ? ' disabled="disabled"' : '';
 
@@ -343,41 +242,51 @@ class Give_Email_Reports_Settings extends Give_Email_Reports {
 			'last'  => 'Last Day',
 		);
 
-		ob_start(); ?>
-		<div class="give-email-reports-monthly">
-			<label class="hidden"
-			       for="<?php echo "{$field->args['id']}_day"; ?>"><?php _e( 'Day of Month', 'give-email-reports' ); ?></label>
+		ob_start();
+		?>
+		<tr valign="top">
+			<?php if ( ! empty( $field['name'] ) && ! in_array( $field['name'], array( '&nbsp;' ) ) ) : ?>
+				<th scope="row" class="titledesc">
+					<label for="<?php echo esc_attr( $field['name'] ); ?>"><?php echo $field['title']; ?></label>
+				</th>
+			<?php endif; ?>
+			<td class="give-forminp">
+				<div class="give-email-reports-monthly">
+					<label class="hidden"
+						   for="<?php echo "{$field['id']}_day"; ?>"><?php _e( 'Day of Month', 'give-email-reports' ); ?></label>
 
-			<select class="cmb2_select" name="<?php echo "{$field->args['id']}[day]"; ?>"
-			        id="<?php echo "{$field->args['id']}_day"; ?>"<?php echo $disabled_field; ?>>
-				<?php
-				// Day select dropdown.
-				foreach ( $days as $day_code => $day ) {
-					$value['day'] = isset( $value['day'] ) ? $value['day'] : '0';
-					echo '<option value="' . $day_code . '" ' . selected( $value['day'], $day_code, true ) . '>' . $day . '</option>';
-				} ?>
-			</select>
+					<select class="cmb2_select" name="<?php echo "{$field['id']}[day]"; ?>"
+							id="<?php echo "{$field['id']}_day"; ?>"<?php echo $disabled_field; ?>>
+						<?php
+						// Day select dropdown.
+						foreach ( $days as $day_code => $day ) {
+							$value['day'] = isset( $value['day'] ) ? $value['day'] : '0';
+							echo '<option value="' . $day_code . '" ' . selected( $value['day'], $day_code, true ) . '>' . $day . '</option>';
+						} ?>
+					</select>
 
-			<label class="hidden"
-			       for="<?php echo "{$field->args['id']}_time"; ?>'"><?php _e( 'Time of Day', 'give-email-reports' ); ?></label>
+					<label class="hidden"
+						   for="<?php echo "{$field['id']}_time"; ?>'"><?php _e( 'Time of Day', 'give-email-reports' ); ?></label>
 
-			<select class="cmb2_select" name="<?php echo "{$field->args['id']}[time]"; ?>"
-			        id="<?php echo "{$field->args['id']}_time"; ?>"<?php echo $disabled_field; ?>>
-				<?php
-				// Time select options.
-				foreach ( $times as $military => $time ) {
-					$value['time'] = isset( $value['time'] ) ? $value['time'] : '1900';
-					echo '<option value="' . $military . '" ' . selected( $value['time'], $military, false ) . '>' . $time . '</option>';
-				} ?>
-			</select>
+					<select class="cmb2_select" name="<?php echo "{$field['id']}[time]"; ?>"
+							id="<?php echo "{$field['id']}_time"; ?>"<?php echo $disabled_field; ?>>
+						<?php
+						// Time select options.
+						foreach ( $times as $military => $time ) {
+							$value['time'] = isset( $value['time'] ) ? $value['time'] : '1900';
+							echo '<option value="' . $military . '" ' . selected( $value['time'], $military, false ) . '>' . $time . '</option>';
+						} ?>
+					</select>
 
-			<?php $this->print_reset_button( 'give_email_reports_monthly_email' ); ?>
+					<?php $this->print_reset_button( 'give_email_reports_monthly_email' ); ?>
 
-			<p class="cmb2-metabox-description"><?php _e( 'Select the day of the month and time that would like to receive the monthly report.', 'give-email-reports' ); ?></p>
+					<p class="give-field-description"><?php _e( 'Select the day of the month and time that would like to receive the monthly report.', 'give-email-reports' ); ?></p>
 
-		</div>
-
-		<?php echo ob_get_clean();
+				</div>
+			</td>
+		</tr>
+		<?php
+		echo ob_get_clean();
 	}
 
 
---
+++ includes/emails/class-monthly-report-email.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * This file has code to handle monthly email reports notification.
+ */
+class Give_Monthly_Email_Notification extends Give_Email_Notification {
+	function init() {
+		$this->load( array(
+			'id'                    => 'monthly-report',
+			'label'                 => __( 'Monthly Email Report', 'give' ),
+			'description'           => __( '', 'give' ),
+			'notification_status'   => 'disabled',
+			'content_type_editable' => false,
+			'content_type'          => 'text/html',
+			// 'form_metabox_setting' => true,
+			'has_recipient_field'   => true,
+			'default_email_subject' => sprintf( __( 'Monthly Donation Report for %1$s', 'give-email-reports' ), get_bloginfo( 'name' ) ),
+			'default_email_message' => $this->get_default_email_message(),
+		) );
+	}
+
+
+	/**
+	 * Get extra setting field.
+	 *
+	 * @access public
+	 *
+	 * @param null $form_id
+	 *
+	 * @return array
+	 */
+	public function get_extra_setting_fields( $form_id = null ) {
+		return array(
+			array(
+				'id'   => 'give_email_reports_monthly_email_delivery_time',
+				'name' => __( 'Monthly Email Delivery Time', 'give-email-reports' ),
+				'desc' => __( 'Select when you would like to receive your monthly email report.', 'give-email-reports' ),
+				'type' => 'email_report_monthly_schedule',
+			)
+		);
+	}
+
+
+	/**
+	 * Get default email message
+	 *
+	 * @access public
+	 * @return string
+	 */
+	public function get_default_email_message() {
+		// $message will be rendered during give_email_message filter.
+		ob_start();
+		give_get_template_part( 'emails/body-report-monthly', Give()->emails->get_template(), true );
+
+		return ob_get_clean();
+	}
+}
+
+return Give_Monthly_Email_Notification::get_instance();
