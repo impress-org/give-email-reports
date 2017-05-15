e2389ecf232bccb3e63358d16e3b0352bb416646
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2017-05-15 15:37:48 +0530

    Add: daily report email notification

---
+++ includes/emails/class-daily-report-email.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * This file has code to handle daily email reports notification.
+ */
+class Give_Daily_Email_Notification extends Give_Email_Notification {
+	function init() {
+		$this->load( array(
+			'id'                    => 'daily-report',
+			'label'                 => __( 'Daily Email Report', 'give' ),
+			'description'           => __( '', 'give' ),
+			'notification_status'   => 'disabled',
+			'content_type_editable' => false,
+			'content_type'          => 'text/html',
+			// 'form_metabox_setting' => true,
+			'has_recipient_field'   => true,
+			'default_email_subject' => sprintf( __( 'Daily Donation Report for %1$s', 'give-email-reports' ), get_bloginfo( 'name' ) ),
+			'default_email_message' => $this->get_default_email_message(),
+		) );
+	}
+
+
+	/**
+	 * Get extra setting field.
+	 *
+	 * @access public
+	 *
+	 * @param null $form_id
+	 *
+	 * @return array
+	 */
+	public function get_extra_setting_fields( $form_id = null ) {
+		return array(
+			array(
+				'id'          => 'give_email_reports_daily_email_delivery_time',
+				'name'        => __( 'Daily Email Delivery Time', 'give-email-reports' ),
+				'desc'        => __( 'Select when you would like to receive your daily email report.', 'give-email-reports' ),
+				'type'        => 'email_report_daily_schedule',
+				'row_classes' => 'cmb-type-email-report-daily-schedule',
+				'default'     => '1900',
+			),
+		);
+	}
+
+
+	/**
+	 * Get default email message
+	 *
+	 * @access public
+	 * @return string
+	 */
+	public function get_default_email_message() {
+		// $message will be rendered during give_email_message filter.
+		ob_start();
+		give_get_template_part( 'emails/body-report-daily', Give()->emails->get_template(), true );
+
+		return ob_get_clean();
+	}
+}
+
+return Give_Daily_Email_Notification::get_instance();
