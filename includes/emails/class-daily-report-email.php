a55829b0aa3a11217d06dbe31a8a82ea95ba2859
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2017-05-16 14:06:44 +0530

    Fix: render report with email template

--- includes/emails/class-daily-report-email.php
+++ includes/emails/class-daily-report-email.php
@@ -18,6 +18,8 @@ class Give_Daily_Email_Notification extends Give_Email_Notification {
 			'default_email_subject' => sprintf( __( 'Daily Donation Report for %1$s', 'give-email-reports' ), get_bloginfo( 'name' ) ),
 			'default_email_message' => $this->get_default_email_message(),
 		) );
+
+		add_filter( 'give_email_notification_setting_fields', array( $this, 'unset_email_setting_field' ), 10, 2 );
 	}
 
 
@@ -33,6 +35,16 @@ class Give_Daily_Email_Notification extends Give_Email_Notification {
 	public function get_extra_setting_fields( $form_id = null ) {
 		return array(
 			array(
+				'id'   => 'give_email_reports_daily_email_template',
+				'name' => __( 'Email template', 'give-email-reports' ),
+				'desc' => __( 'Choose your template from the available registered template types.', 'give-email-reports' ),
+				'type' => 'select',
+				'default' => 'report-daily',
+				'options' => array(
+					'report-daily' => __( 'Daily Report', 'give-email-reports' ),
+				),
+			),
+			array(
 				'id'          => 'give_email_reports_daily_email_delivery_time',
 				'name'        => __( 'Daily Email Delivery Time', 'give-email-reports' ),
 				'desc'        => __( 'Select when you would like to receive your daily email report.', 'give-email-reports' ),
@@ -45,6 +57,29 @@ class Give_Daily_Email_Notification extends Give_Email_Notification {
 
 
 	/**
+	 * unset email message setting field.
+	 *
+	 * @access public
+	 * @param array                   $settings
+	 * @param Give_Email_Notification $email
+	 *
+	 * @return array
+	 */
+	public function unset_email_setting_field( $settings, $email ) {
+		if ( $this->config['id'] === $email->config['id'] ) {
+			foreach ( $settings as $index => $setting ) {
+				if ( "{$this->config['id']}_email_message" === $setting['id'] ) {
+					unset( $settings[ $index ] );
+					break;
+				}
+			}
+		}
+
+		return array_values( $settings );
+	}
+
+
+	/**
 	 * Get default email message
 	 *
 	 * @access public
@@ -53,7 +88,7 @@ class Give_Daily_Email_Notification extends Give_Email_Notification {
 	public function get_default_email_message() {
 		// $message will be rendered during give_email_message filter.
 		ob_start();
-		give_get_template_part( 'emails/body-report-daily', Give()->emails->get_template(), true );
+		give_get_template_part( 'emails/body', give_get_option( 'give_email_reports_daily_email_template', 'report-daily' ), true );
 
 		return ob_get_clean();
 	}
--- includes/emails/class-weekly-report-email.php
+++ includes/emails/class-weekly-report-email.php
@@ -18,6 +18,8 @@ class Give_Weekly_Email_Notification extends Give_Email_Notification {
 			'default_email_subject' => sprintf( __( 'Weekly Donation Report for %1$s', 'give-email-reports' ), get_bloginfo( 'name' ) ),
 			'default_email_message' => $this->get_default_email_message(),
 		) );
+
+		add_filter( 'give_email_notification_setting_fields', array( $this, 'unset_email_setting_field' ), 10, 2 );
 	}
 
 
@@ -33,12 +35,45 @@ class Give_Weekly_Email_Notification extends Give_Email_Notification {
 	public function get_extra_setting_fields( $form_id = null ) {
 		return array(
 			array(
+				'id'   => 'give_email_reports_weekly_email_template',
+				'name' => __( 'Email template', 'give-email-reports' ),
+				'desc' => __( 'Choose your template from the available registered template types.', 'give-email-reports' ),
+				'type' => 'select',
+				'default' => 'report-weekly',
+				'options' => array(
+					'report-weekly' => __( 'Weekly Report', 'give-email-reports' ),
+				),
+			),
+			array(
 				'id'   => 'give_email_reports_weekly_email_delivery_time',
 				'name' => __( 'Weekly Email Delivery Time', 'give-email-reports' ),
 				'desc' => __( 'Select when you would like to receive your weekly email report.', 'give-email-reports' ),
 				'type' => 'email_report_weekly_schedule',
-			)
+			),
 		);
+	}
+
+
+	/**
+	 * unset email message setting field.
+	 *
+	 * @access public
+	 * @param array                   $settings
+	 * @param Give_Email_Notification $email
+	 *
+	 * @return array
+	 */
+	public function unset_email_setting_field( $settings, $email ) {
+		if ( $this->config['id'] === $email->config['id'] ) {
+			foreach ( $settings as $index => $setting ) {
+				if ( "{$this->config['id']}_email_message" === $setting['id'] ) {
+					unset( $settings[ $index ] );
+					break;
+				}
+			}
+		}
+
+		return array_values( $settings );
 	}
 
 
@@ -51,7 +86,7 @@ class Give_Weekly_Email_Notification extends Give_Email_Notification {
 	public function get_default_email_message() {
 		// $message will be rendered during give_email_message filter.
 		ob_start();
-		give_get_template_part( 'emails/body-report-weekly', Give()->emails->get_template(), true );
+		give_get_template_part( 'emails/body', give_get_option( 'give_email_reports_daily_email_template', 'report-weekly' ), true );
 
 		return ob_get_clean();
 	}
