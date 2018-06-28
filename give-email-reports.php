cd5f5f5058156e79d68c9ad05506612bc6e582ca
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2018-06-28 15:11:54 +0530

    update plugin version to 1.1.2

--- give-email-reports.php
+++ give-email-reports.php
@@ -3,7 +3,7 @@
  * Plugin Name:     Give - Email Reports
  * Plugin URI:      https://givewp.com/addons/email-reports/
  * Description:     Receive comprehensive donations reports via email.
- * Version:         1.2.0
+ * Version:         1.1.2
  * Author:          WordImpress
  * Author URI:      https://wordimpress.com
  * Text Domain:     give-email-reports
@@ -17,7 +17,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 // Plugin version.
 if ( ! defined( 'GIVE_EMAIL_REPORTS_VERSION' ) ) {
-	define( 'GIVE_EMAIL_REPORTS_VERSION', '1.2.0' );
+	define( 'GIVE_EMAIL_REPORTS_VERSION', '1.1.2' );
 }
 
 // Min. Give Core version.
